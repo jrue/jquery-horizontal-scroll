a05d9b38856c025ab400cb1178fa094011d99555
Jeremy Rue <jrue@users.noreply.github.com>
2021-04-30 01:51:39 -0700

    first commit

---
+++ dist/jquery-horizontal-scroll.js
@@ -0,0 +1,184 @@
+/**************************
+jQuery Horizontal Scroll
+
+MIT LICENSE: Copyright 2021 Jeremy Rue https://github.com/jrue
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**************************/
+
+
+(function($){
+  
+  $.horizontalScroll = function(el, options){
+
+    const base = this;
+    
+    // Access to jQuery and DOM versions of element
+    base.$el = $(el);
+    base.el = el;
+    
+    
+    base.numOfItems = $(el).children().length;
+    base.sumWidths = base.numOfItems * $(el).width();
+    base.maxHeight = 0;
+
+    
+    $(el).children().each(function(){
+
+      //get the tallest item, and use that as the standard (they can override)
+      base.maxHeight = Math.max($(this).height(), base.maxHeight);
+
+      //keep things from resizing after we build the strip
+      $(this).css({
+        "height": $(this).outerHeight(),
+        "width": $(this).outerWidth(),
+        "display": "block"
+      });
+    });
+    
+    //get any padding for absolute adjustment (they can override)
+    base.paddingTop = $(el).css('padding-top');
+    base.paddingLeft = $(el).css('padding-left');
+    
+    //default options
+    $.horizontalScroll.defaultOptions = {
+        "containerHeight": "300vh",
+        "itemsHeight": base.maxHeight,
+        "width": base.sumWidths.toString() + "px",
+        "paddingTop": base.paddingTop,
+        "paddingLeft": base.paddingLeft
+    };
+
+    //merge options passed in during instantiation with our defaults
+    base.options = $.extend({},$.horizontalScroll.defaultOptions, options);
+    
+    
+    base.whenResizedCaptureNewItemSizes = ()=>{
+      
+      base.maxHeight = 0;
+      
+      $(el).find('.strip-holder').children().each(function(){
+        
+        let ratio = $(this).height() / $(this).width();
+        base.maxHeight = Math.max($(el).width() * ratio, base.maxHeight);
+        
+        $(this).css({
+          "height": Math.ceil($(el).width() * ratio),
+          "width": $(el).width(),
+          "display": "block"
+        });
+      });
+      
+      $(el).find(".strip-holder").css({
+        "height": base.maxHeight,
+        "width": base.numOfItems * $(el).width()
+      });
+      
+      $(el).find(".sticky-holder").css({
+        "height": base.maxHeight,
+        "width": $(el).width()
+      })
+      
+      base.topScrollTrigger = $(el).find('.strip-holder').offset().top + ($(el).find('.strip-holder').height()/2);
+      base.bottomScrollTrigger = $(el).offset().top + $(el).height() - ($(el).find('.strip-holder').height()/2);
+    }
+    
+    //trigger resize after 200ms from when they resize window
+    $(window).on('resize', ()=>{
+      clearTimeout(base.resizeTimer);
+      base.resizeTimer = window.setTimeout(base.whenResizedCaptureNewItemSizes, 200);
+    })
+    
+
+    base.init = ()=>{
+      
+      //set properties of container
+      $(el).css({
+        "height": base.options.containerHeight,
+        "position": "relative",
+        "padding": 0,
+        "overflow":"visible"
+      });
+      
+      
+      let holder1 = $("<div />")
+        .addClass("sticky-holder")
+        .css({
+          "position":"sticky",
+          "overflow":"hidden",
+          "top": "50%",
+          "left": 0,
+          "transform":"translateY(-50%)",
+          "width": $(el).width(),
+          "height": base.options.itemsHeight
+        });
+      
+      
+      //create holder
+      let holder2 = $("<div />")
+        .addClass("strip-holder")
+        .css({
+          "position":"absolute",
+          "height": base.options.itemsHeight,
+          "top": base.options.paddingTop,
+          "left": base.options.paddingLeft,
+          "width": base.options.width
+        });
+      
+      //go through each child item, set float left
+      $(el).children().each(function(){
+        $(this).css("float","left");
+        $(this).detach().appendTo(holder2);
+      })
+      
+      holder2.appendTo(holder1);
+      holder1.appendTo($(el));
+      
+      base.topScrollTrigger = $(el).find('.strip-holder').offset().top + ($(el).find('.strip-holder').height()/2);
+      base.bottomScrollTrigger = $(el).offset().top + $(el).height() - ($(el).find('.strip-holder').height()/2);
+      
+      $(window).on("scroll", base.onScroll)
+
+    };
+    
+    base.onScroll = ()=>{
+      
+      let currScrollTop = $(window).scrollTop() + ($(window).height()/2);
+      let startOfScroll = $(el).offset().top;
+      let endOfScroll = $(el).offset().top + $(el).height() - $(".sticky-holder").height();
+      let totalDistance = endOfScroll - startOfScroll;
+      let percentScrolled = Math.min(Math.max(currScrollTop - startOfScroll,0) / totalDistance, 1);
+      let stripHolderWidth = $(el).find(".strip-holder").width() - $(el).find(".strip-holder").children(":last-child").width();
+      
+      //before sticky
+      if(currScrollTop < startOfScroll){
+        $(".strip-holder").css("transform", "translate3d(0px,0px,0px)");
+      }
+      
+      //trigger when stickiness has been applied
+      if(currScrollTop > startOfScroll && currScrollTop < endOfScroll){
+        $(".strip-holder").css("transform", `translate3d(-${percentScrolled * stripHolderWidth}px,0px,0px)`);
+      }
+      
+      //trigger when stickiness is done
+      if(currScrollTop > endOfScroll){
+        $(".strip-holder").css("transform", `translate3d(-${stripHolderWidth}px,0px,0px)`);
+      }
+    }
+
+    // Run initializer
+    base.init();
+  };
+  
+
+  $.fn.horizontalScroll = function(options){
+    return this.each(function(){
+      const horizontalScroll  = (new $.horizontalScroll(this, options));
+    });
+  };
+    
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,60 @@
+jQuery Horiztonal Scroll Gallery
+-----------------------------
+
+This jQuery plugin slides through a horiztonal gallery of items as you scroll down. It makes use of position:sticky so that the gallery briefly fixes in place as it scrolls horizontally [See example here (scroll down the page)](https://jrue.github.io/jquery-horizontal-scroll/example/)
+
+Ready to download? Get the minified JS file here:
+[https://github.com/jrue/jquery-horizontal-scroll/blob/main/dist/jquery-horizontal-scroll.min.js](https://github.com/jrue/jquery-horizontal-scroll/blob/main/dist/jquery-horizontal-scroll.min.js)
+
+#### Usage:
+
+The gallery need to be an HTML element with some child elements one-level down. It will take those first-level children, and automatically adjust them into a filmstrip element with all of the correct CSS, so there is little to do other than add the HTML. The child elements can be images, or div tags, or whatever. The film strip's height will be based on the tallest element it finds. 
+
+
+#### Example HTML:
+
+```html
+<div class="horizontal-scroll">
+  <div class="example">
+    <img src="images/desk-1.jpg" class="img-fluid" alt="desk">
+  </div>
+  <div class="example">
+    <img src="images/desk-2.jpg" class="img-fluid" alt="desk">
+  </div>
+  <div class="example">
+    <img src="images/desk-3.jpg" class="img-fluid" alt="desk">
+  </div>
+  <div class="example">
+    <img src="images/desk-4.jpg" class="img-fluid" alt="desk">
+  </div>
+</div>
+```
+
+This plugin requires jQuery (tested with 3x, but should work on earlier versions) and will need to be loaded after the jQuery library. 
+
+```html
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="jquery-horizontal-scroll.min.js"></script>
+```
+
+Target the CSS class of your containing element and run the `horizontalScroll()` method. (In the following example it's `.horizontal-scroll`) and set the options you want. The main option is how tall you want the containing element. The shorter it is, the more quickly it will scroll through the gallery. It defaults to **300vh** which is 300 percent of the viewport height. You can try different numbers to see how it affects the scroll. 
+
+Larger numbers require more scroll effort to slide the gallery, and thus results in a slower scrolling gallery. 
+
+```javascript
+$( '.horizontal-scroll' ).horizontalScroll({
+  containerHeight: "300vh"
+});
+```
+
+#### Additional Options
+
+You shouldn't need to add these additional options. In most cases they're auto detected. But I put them here just in case someone wanted to override the defaults. 
+
+**containerHeight** - The height of the container, which requires more scrolling.
+**itemsHeight** - Automatically based on the tallest item it finds, but you can set it yourself. An overflow hidden property will chop off anything protruding if you set it smaller than an element.
+**width** - Width of the strip of items. Automatically based on number of items multipled by the width of the main container. You can override here.
+**paddingTop** - Auto detects if you had padding on the container. But this will affect the point in which the stickiness catches.
+**paddingLeft** - Auto detects, but if you had padding-left on the container, it will ensure it carries over from the starting location of the film strip.
+
+MIT License
